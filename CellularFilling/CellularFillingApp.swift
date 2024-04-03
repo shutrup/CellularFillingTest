47594592e769938aeac82e4c949442819df1a8f9
shutrup <magagaraev95@mail.ru>
2024-04-03 20:18:20 +0300

    Initial Commit

---
+++ CellularFilling/CellularFillingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellularFillingApp.swift
+//  CellularFilling
+//
+//  Created by Шарап Бамматов on 03.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CellularFillingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
